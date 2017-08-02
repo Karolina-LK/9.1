f361faef07750bac9e1bbf77f8d2241bfaa41d78
Karolina Lubieniecka <lubieniecka@interia.pl>
2017-08-02 14:40:36 +0200

    added changed js file

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
  var a,
-     h;
+      h;
 
- function getTriangleArea(a, h) {
+function getTriangleArea(a, h) {
  if ( (a<=0) || (h<=0) )   {console.log("Nieprawidłowe dane")
 } 
 
   return a*h/2;
 }
-	
+
 var triAngle1Area = getTriangleArea (10,5);
 console.log( getTriangleArea(10,6) ) 
 
@@ -15,5 +15,4 @@ var triAngle2Area = getTriangleArea (8,2);
 console.log( getTriangleArea(8,2) );
 
 var triAngle3Area = getTriangleArea (20,8);
-console.log( getTriangleArea(20,8) );
-
+console.log( triAngle3Area );
